dad18965ac7ef7f6ae822f8e6194002067a4d390
andr3sp <andr3spulgarin@gmail.com>
2020-05-08 19:58:16 -0500

    a

--- lib/screens/page1.dart
+++ lib/screens/page1.dart
@@ -9,15 +9,20 @@ import 'package:provider_firestore/screens/page2.dart';
  
  class _HomePageState extends State<HomePage> {
 
-   int total = 0;
-   
-  Future _data;
-  Future getPosts() async {
-    var firestore = Firestore.instance;
-    QuerySnapshot qn = await firestore.collection('characters').getDocuments();
-      return qn.documents;
-  }
+int total = 0;
   
+Future _data;
+  Future getPosts() async {
+    final query = await Firestore.instance.collection('characters').getDocuments();
+    final futures = query.documents.map((doc) async {
+
+      var map = doc.data;
+      map['total_horas'] = await countHoras(doc.documentID);
+      return map;
+    });
+    return Future.wait(futures);
+  }
+ 
   Future<int> countHoras(String empleadoId) async {
                       final query = await Firestore.instance
                           .collection('characters')
@@ -47,36 +52,37 @@ import 'package:provider_firestore/screens/page2.dart';
           appBar: AppBar(backgroundColor: Colors.blueGrey,
           title: Text('Heroes', style: TextStyle(fontSize: 32,)), ),    
           body:Container(
-             child: FutureBuilder(
-             future: _data,
-             builder: (_, snapshot){ 
-             if (snapshot.connectionState == ConnectionState.waiting) {
-               return Center(
-                 child: Column( mainAxisAlignment: MainAxisAlignment.center,
-                   children: <Widget>[CircularProgressIndicator(), Text('Please wait...'),],),
-               );
-             } else {
+            child: FutureBuilder(
+            future: _data,
+            builder: (_, snapshot){ 
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: Column( mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[CircularProgressIndicator(), Text('Please wait...'),],),
+              );
+            } else {
               return ListView.builder(
-                 itemCount: snapshot.data.length,
-                 itemBuilder: (_, index,) async 
-
-                   final horas = await  countHoras(snapshot.data[index].documentID);
-                 
-
-                    return Card(color: Colors.white54,
-                            child: ListTile(
-                              leading: Icon(Icons.account_circle, size: 40.0,),
-                              title: Text(snapshot.data[index].data['name']),
-                              trailing: Text("Total horas:$horas"),
-                                //   SEND DATA
-                              onTap: () => navigateToDetail(snapshot.data[index]),
-                     ),
-                   );
-                
-               });
-             }
-           }),
-    ),
-       );
-   }
- }
+                itemCount: snapshot.data.length,
+                itemBuilder: (_, index,){
+                    return Card(
+                        color: Colors.white54,
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.account_circle,
+                            size: 40.0,
+                          ),
+                          title: Text(snapshot.data[index]['name']),
+                          trailing: Text(
+                              "Total horas:${snapshot.data[index]['total_horas']}"),
+                          //   SEND DATA
+                          onTap: () => navigateToDetail(snapshot.data[index]),
+                        ),
+                      );
+                  
+                });
+              }
+            }),
+      ),
+        );
+    }
+  }
